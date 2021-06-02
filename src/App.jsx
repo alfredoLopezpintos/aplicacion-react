986ab9f6681ff6e9c48ae6cb1b22f045cd0b54c0
Alfredo <alfredodaniel87617@gmail.com>
2021-06-01 22:39:15 -0300

    This is a gitflow test

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
           <code>src/App.js</code>
           and save to reload.
         </p>
+        <p> TEST </p>
         <a
           className="App-link"
           href="https://reactjs.org"
