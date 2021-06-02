e640f014e7def2bcde1a3811bc90332dee58d261
Alfredo <alfredodaniel87617@gmail.com>
2021-06-01 23:29:43 -0300

    Fixed test

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
           <code>src/App.js</code>
           and save to reload.
         </p>
-        <p> PRUEBA </p>
         <a
           className="App-link"
           href="https://reactjs.org"
