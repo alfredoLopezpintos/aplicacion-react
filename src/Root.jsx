f45075efead5ed3a540219e3cdf82d39d17a9eb2
Alfredo <alfredodaniel87617@gmail.com>
2021-06-05 21:18:36 -0300

    Worked on react-router. Fixed folder order

---
+++ src/Root.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// // export const Root = () => (
+// //   <BrowserRouter>
+// //     <Switch>
+// //       <Route exact path="/" />
+// //     </Switch>
+// //   </BrowserRouter>
+// // );
