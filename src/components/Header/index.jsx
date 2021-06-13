fd33ccdce40414cb03e73669a2ba7c832f699039
Alfredo <alfredodaniel87617@gmail.com>
2021-06-13 20:47:05 -0300

    Added trashBin icon feature

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { MdDelete } from 'react-icons/md';
+import { MdDelete, MdDeleteForever } from 'react-icons/md';
 import './Header.css';
 
-export const Header = () => (
-  <div className="header">
-    <Link className="home" to="/">
-      <b>PostITs</b>
-    </Link>
-    <Link className="trash" to="/TrashBin">
-      <MdDelete className="trashIcon" />
-      <b>Trash Bin</b>
-    </Link>
-  </div>
-);
+export const Header = () => {
+  const { posts } = useSelector((state) => state.postITs);
+  const deltedPosts = posts.filter((post) => post.deleted);
+  const result = ((deltedPosts.length !== 0) && (<MdDeleteForever className="trashIcon" />));
+  const result2 = ((deltedPosts.length === 0) && (<MdDelete className="trashIcon" />));
+  return (
+    <div className="header">
+      <Link className="home" to="/">
+        <b>PostITs</b>
+      </Link>
+      <Link className="trash" to="/TrashBin">
+        {result}
+        {result2}
+        <b>Trash Bin</b>
+      </Link>
+    </div>
+  );
+};
 
 export default Header;
